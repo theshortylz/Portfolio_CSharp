04a1fe03a56a0d9be5be49f8214ae6722045db95
Mateo Jaramillo <mateoloro1730@outlook.com>
2023-11-12 22:39:53 -0500

    Adding Substract & Division Methods

--- calculator/calculator/Form1.cs
+++ calculator/calculator/Form1.cs
@@ -6,5 +6,17 @@ namespace calculator
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private int Substract(int a, int b) { return (a-b); }
+        private float Division(int a, int b) 
+        {
+            if (b == 0) { throw new DivideByZeroException(); }
+            return a / b;
+        }
     }
 }
