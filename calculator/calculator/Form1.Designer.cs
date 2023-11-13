6b456b358669a8a61feb8b14ec6cc4e72579f325
JuanEstebanDream <137835852+JuanEstebanDream@users.noreply.github.com>
2023-11-12 23:11:56 -0500

    new buttons stashed changes

--- calculator/calculator/Form1.Designer.cs
+++ calculator/calculator/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+<<<<<<< Updated upstream
             SuspendLayout();
             // 
             // Form1
@@ -35,6 +36,199 @@
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+=======
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            button15 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(81, 225);
+            button1.Name = "button1";
+            button1.Size = new Size(123, 47);
+            button1.TabIndex = 0;
+            button1.Text = "1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(210, 225);
+            button2.Name = "button2";
+            button2.Size = new Size(117, 47);
+            button2.TabIndex = 1;
+            button2.Text = "2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(333, 225);
+            button3.Name = "button3";
+            button3.Size = new Size(113, 47);
+            button3.TabIndex = 2;
+            button3.Text = "3";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(81, 278);
+            button4.Name = "button4";
+            button4.Size = new Size(123, 49);
+            button4.TabIndex = 3;
+            button4.Text = "4";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(210, 278);
+            button5.Name = "button5";
+            button5.Size = new Size(117, 49);
+            button5.TabIndex = 4;
+            button5.Text = "5";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(333, 281);
+            button6.Name = "button6";
+            button6.Size = new Size(113, 49);
+            button6.TabIndex = 5;
+            button6.Text = "6";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(81, 336);
+            button7.Name = "button7";
+            button7.Size = new Size(123, 45);
+            button7.TabIndex = 6;
+            button7.Text = "7";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(210, 336);
+            button8.Name = "button8";
+            button8.Size = new Size(117, 43);
+            button8.TabIndex = 7;
+            button8.Text = "8";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button8_Click;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(333, 336);
+            button9.Name = "button9";
+            button9.Size = new Size(113, 43);
+            button9.TabIndex = 8;
+            button9.Text = "9";
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button9_Click;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(210, 385);
+            button10.Name = "button10";
+            button10.Size = new Size(123, 47);
+            button10.TabIndex = 9;
+            button10.Text = "0";
+            button10.UseVisualStyleBackColor = true;
+            button10.Click += button10_Click;
+            // 
+            // button11
+            // 
+            button11.BackColor = Color.CornflowerBlue;
+            button11.Location = new Point(465, 278);
+            button11.Name = "button11";
+            button11.Size = new Size(117, 49);
+            button11.TabIndex = 10;
+            button11.Text = "-";
+            button11.UseVisualStyleBackColor = false;
+            button11.Click += button11_Click;
+            // 
+            // button12
+            // 
+            button12.BackColor = Color.FromArgb(192, 0, 0);
+            button12.Location = new Point(469, 223);
+            button12.Name = "button12";
+            button12.Size = new Size(113, 49);
+            button12.TabIndex = 11;
+            button12.Text = "+";
+            button12.UseVisualStyleBackColor = false;
+            button12.Click += button12_Click;
+            // 
+            // button13
+            // 
+            button13.BackColor = Color.FromArgb(255, 128, 0);
+            button13.Location = new Point(469, 336);
+            button13.Name = "button13";
+            button13.Size = new Size(113, 45);
+            button13.TabIndex = 12;
+            button13.Text = "*";
+            button13.UseVisualStyleBackColor = false;
+            button13.Click += button13_Click;
+            // 
+            // button14
+            // 
+            button14.BackColor = Color.FromArgb(192, 0, 192);
+            button14.Location = new Point(469, 385);
+            button14.Name = "button14";
+            button14.Size = new Size(113, 47);
+            button14.TabIndex = 13;
+            button14.Text = "/";
+            button14.UseVisualStyleBackColor = false;
+            button14.Click += button14_Click;
+            // 
+            // button15
+            // 
+            button15.BackColor = SystemColors.InactiveCaption;
+            button15.Location = new Point(339, 385);
+            button15.Name = "button15";
+            button15.Size = new Size(107, 47);
+            button15.TabIndex = 14;
+            button15.Text = "=";
+            button15.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button15);
+            Controls.Add(button14);
+            Controls.Add(button13);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+>>>>>>> Stashed changes
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
@@ -42,5 +236,21 @@
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+        private Button button15;
     }
 }
--- calculator/calculator/Form1.cs
+++ calculator/calculator/Form1.cs
@@ -9,6 +9,7 @@ namespace calculator
 
         private void Form1_Load(object sender, EventArgs e)
         {
+<<<<<<< Updated upstream
 
         }
 
@@ -17,6 +18,79 @@ namespace calculator
         {
             if (b == 0) { throw new DivideByZeroException(); }
             return a / b;
+=======
+            Button button = (Button)sender;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+
+>>>>>>> Stashed changes
         }
     }
 }
